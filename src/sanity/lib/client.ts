2765aa56ac9973aa830ac6f3a57c96d51dacf166
FiZaRafakat <164252341+FiZaRafakat@users.noreply.github.com>
2024-12-28 23:01:23 -0800

    sanity

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,11 +1,11 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId ,token } from '../env'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: true, 
-  token : "skBr6RCivebVmL5ypuCu7mVj6qRln340Y9olKbtWzSjO3aNwbaINpW1OzyDr2QMnkHaGEW5VqnfugaJyj1LocP8nBEE5KejlVF9ou8MeabZI55j44cTJeckyuqoFsjOIkEd98nSRkt6pepLfYpO5aDsrgYbOUVwpdxEMBK7gMwouFxv69HBB" ,
+  token :  "skBr6RCivebVmL5ypuCu7mVj6qRln340Y9olKbtWzSjO3aNwbaINpW1OzyDr2QMnkHaGEW5VqnfugaJyj1LocP8nBEE5KejlVF9ou8MeabZI55j44cTJeckyuqoFsjOIkEd98nSRkt6pepLfYpO5aDsrgYbOUVwpdxEMBK7gMwouFxv69HBB" ,
 })
