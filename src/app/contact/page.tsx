5beeb5936869649b2b75e180a7fa4636f0455f16
FiZaRafakat <164252341+FiZaRafakat@users.noreply.github.com>
2024-12-30 01:10:24 -0800

    update read me

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -23,12 +23,12 @@ const Page = () => {
   return (
     <div className='bg-[#f3f1f6] text-gray-900'>
         <Header />
-        <div className='md:w-[600px] sm:w-[500px] min-[500px]:w-[450px] min-[400px]:w-[380px] min-[300px]:w-[280px] w-[200px] mx-auto pb-16 pt-10 '>
+        <div className='md:w-[600px] sm:w-[500px] min-[500px]:w-[450px] px-10  mx-auto pb-16 pt-10 '>
           <div className='lg:mx-12 md:mx-5 mb-5'>
-          <h2 className='font-medium text-lg text-center'>At <span className='font-bold italic text-xl'>DizzyBlog</span> , we love to connect, inspire, and grow together. Reach out to share your thoughts, ideas, or just to say hi – because every great story starts with a conversation ! </h2>
+          <h2 className='font-medium text-xl tracking-wider text-center'>At <span className='font-bold italic text-2xl'>DizzyBlog</span> , we love to connect, inspire, and grow together. Reach out to share your thoughts, ideas, or just to say hi – because every great story starts with a conversation ! </h2>
           </div>
          {/* Form Div */}
-         <form action="" method='POST' className=" md:space-y-1 flex flex-col shadow-[#113946] shadow-md lg:px-8 lg:mx-12 md:mx-5 sm:px-8 px-3 rounded-xl md:pb-10 md:pt-10 pt-5 pb-2 gap-2">
+         <form action="" method='POST' className=" md:space-y-1 flex flex-col shadow-[#113946] shadow-md lg:px-8 lg:mx-12 md:mx-5 text-lg tracking-wide sm:px-8 px-3 rounded-xl md:pb-10 md:pt-10 pt-5 pb-2 gap-2">
 
           <label htmlFor="name" className="flex flex-col gap-1">
             <p className="text-[16px] font-medium">Your Name</p>
@@ -39,11 +39,6 @@ const Page = () => {
             <p className="text-[16px] font-medium">Email Address</p>
             <input type="text" name="email" id="email" placeholder="fiza@gmail.com" className="focus:border focus:border-[#113946] bg-gray-200 border-gray-100 border placeholder:text-gray-600 min-[400px]:text-base text-sm w-full min-[400px]:px-4 px-2 py-2 min-[400px]:rounded-xl rounded-md outline-none " />
           </label>
-         
-         {/* <label htmlFor="Subject" className="flex flex-col gap-2">
-          <p className="text-[16px] font-medium">Subject</p>
-            <input type="text" name="Subject" id="Subject" placeholder="This is an optional" className="focus:border focus:border-[#113946] border-gray-100 border bg-gray-200 placeholder:text-gray-600 min-[400px]:text-base text-sm w-full min-[400px]:px-4 px-2 py-2 min-[400px]:rounded-xl rounded-md outline-none " />
-          </label> */}
         
           <label htmlFor="msg" className="flex flex-col gap-1">
             <p className="text-[16px] font-medium">Message</p>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,9 +12,9 @@ const Hero = () => {
     },
     {
       src: "bookwithglasses.png",
-      position: "xl:bottom-28 bottom-60 xl:right-[320px] right-[100px]  ",
+      position: "xl:bottom-28 bottom-[270px] xl:right-[320px] right-[85px]  ",
       size: "xl:h-[170px] h-[180px] max-[500px]:h-[140px] ",
-      delay: "0.5s",
+      delay: "0s",
     },
     {
       src: "books.png",
@@ -24,9 +24,9 @@ const Hero = () => {
     },
     {
       src: "cup.png",
-      position: "xl:bottom-7 bottom-4  right-0 sm:right-24 min-[460px]:right-10 xl:right-0",
+      position: "xl:bottom-7 bottom-10  right-0 sm:right-24 min-[460px]:right-10 xl:right-0",
       size: "xl:h-[350px] lg:h-[280px]  h-[250px] max-[500px]:h-[225px]",
-      delay: "0.5s",
+      delay: "0s",
     },
     {
       src: "book2.png",
@@ -38,46 +38,46 @@ const Hero = () => {
       src: "pen2.png",
       position: "md:bottom-20 md:right-[300px] lg:right-[350px] xl:right-[520px]",
       size: "xl:h-[180px] lg:h-[190px] hidden md:block h-[120px]",
-      delay: "0.5s",
+      delay: "0s",
     },
   ];
 
   const secondSet = [
     {
       src: "book-2.png",
-      position: "xl:bottom-12 hidden md:block md:bottom-48 lg:bottom-44  lg:left-[270px] md:left-[230px] xl:left-auto xl:right-[340px]",
+      position: "xl:bottom-12 lg:bottom-44 sm:bottom-60 md:bottom-28 lg:left-[270px] md:left-[250px] left-auto right-5 xl:left-auto xl:right-[340px]",
       size: "xl:h-[250px] md:h-[140px] lg:h-[180px] transform rotate-[30deg]",
       delay: "0s",
     },
     {
       src: "beautiful-book.png",
       position: "bottom-0  right-0  xl:left-[250px]",
-      size: "min-[400px]:h-[160px] min-[500px]:h-[190px] md:h-[250px] h-[150px]",
+      size: "h-[300px]  md:h-[250px] ",
       delay: "0s",
     },
     {
       src: "images.png",
-      position: "md:bottom-0 hidden min-[470px]:block right-[100px] md:right-[250px]  sm:right-[200px] bottom-20 min-[540px]:right-[150px]  lg:right-[470px]",
-      size: "xl:h-[250px] md:h-[230px] sm:h-[190px] ",
-      delay: "0.5s",
+      position: "md:bottom-0 hidden lg:bottom-16 md:block md:right-[250px]  lg:right-[470px]",
+      size: "md:h-[250px]",
+      delay: "0s",
     },
     {
       src: "writing.png",
-      position: "lg:bottom-24 sm:bottom-44 lg:bottom-48 min-[500px]:bottom-44 md:bottom-40 bottom-32 left-0",
-      size: "xl:h-auto lg:h-[240px] sm:h-[180px] md:h-[200px] min-[500px]:h-[180px] h-[150px]",
+      position: "lg:bottom-20 sm:bottom-44 lg:bottom-48 bottom-52 md:bottom-40 left-0",
+      size: "xl:h-auto lg:h-[240px] sm:h-[280px]  h-[250px] ",
       delay: "0s",
     },
     {
       src: "writing2.png",
-      position: "bottom-0 left-5 min-[400px]:left-5 min-[400px]:right-auto md:left-[60px] lg:left-auto lg:right-[300px] xl:right-0",
-      size: "xl:h-auto lg:h-[220px] min-[500px]:h-[180px] sm:h-[200px] md:h-[180px] h-[140px]",
+      position: "hidden sm:block bottom-0 min-[400px]:left-5 min-[400px]:right-auto md:left-[60px] lg:left-auto lg:right-[300px] xl:right-0",
+      size: "xl:h-auto lg:h-[220px] min-[500px]:h-[180px] sm:h-[200px] md:h-[180px]",
       delay: "0s",
     },
     {
       src: "pen.png",
-      position: "lg:bottom-5 bottom-32 sm:bottom-48 min-[500px]:right-[100px] min-[500px]:bottom-40  max-[500px]:right-6 sm:right-20 lg:left-0",
+      position: "lg:bottom-5 bottom-32 hidden md:block sm:bottom-48 sm:right-20 lg:left-0",
       size: "transform lg:-rotate-[90deg] rotate-[7deg] sm:h-[170px] h-[120px] min-[500px]:h-[130px] lg:h-[200px]",
-      delay: "lg:0.5s 0s",
+      delay: "0s",
     },
   ];
 
